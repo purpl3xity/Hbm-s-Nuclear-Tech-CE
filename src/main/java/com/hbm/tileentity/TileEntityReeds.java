954360f12fce7061f12e70d7d09f42b10394af28
Vladimir <68227685+NoPressF@users.noreply.github.com>
2026-03-11 18:44:35 +0100

    fix: meteorite alloyed sword put in slot battery in assembly machine & increase max render distance squared for reeds

--- src/main/java/com/hbm/tileentity/TileEntityReeds.java
+++ src/main/java/com/hbm/tileentity/TileEntityReeds.java
@@ -3,6 +3,8 @@ package com.hbm.tileentity;
 import com.hbm.interfaces.AutoRegister;
 import net.minecraft.tileentity.TileEntity;
 import net.minecraft.util.math.AxisAlignedBB;
+import net.minecraftforge.fml.relauncher.Side;
+import net.minecraftforge.fml.relauncher.SideOnly;
 import org.jetbrains.annotations.NotNull;
 
 @AutoRegister
@@ -10,5 +12,11 @@ public class TileEntityReeds extends TileEntity {
     @Override
     public @NotNull AxisAlignedBB getRenderBoundingBox() {
         return INFINITE_EXTENT_AABB;
+    }
+
+    @Override
+    @SideOnly(Side.CLIENT)
+    public double getMaxRenderDistanceSquared() {
+        return 65536.0D;
     }
 }
